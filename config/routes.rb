0bb30c732dd2a7f9f2421b99abe2926a12592df3
Josh Burd <Jburdt@gmail.com>
2023-02-06 15:54:15 -0500

    commit tree 4d47b5555ca0d31b464f99c076dbc64873771a1e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "students", to: "students#index"
 end
+
+Rails.application.routes.draw do
+  get "students/grades", to: "students#grades"
+end
+
+Rails.application.routes.draw do
+  get "students/highest-grade", to: "students#highest_grade"
+end
+
